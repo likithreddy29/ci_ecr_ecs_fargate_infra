1592446fd445ed1c0aad1af754f427624c17d672
likithreddy29 <57021218+likithreddy29@users.noreply.github.com>
2026-07-30 20:45:47 -0400

    Add ECS cluster and service output variables
    Added outputs for ECS cluster and service names.

--- outputs.tf
+++ outputs.tf
@@ -7,3 +7,15 @@ output "repository_arn" {
   value       = aws_ecr_repository.app_repo.arn
   description = "The ARN of the created ECR repository"
 }
+
+## ECS 
+
+output "ecs_cluster_name" {
+  value       = aws_ecs_cluster.zomato_cluster.name
+  description = "Name of the created ECS Cluster"
+}
+
+output "ecs_service_name" {
+  value       = aws_ecs_service.zomato_service.name
+  description = "Name of the created ECS Service"
+}
