3ff41b8afd3ff997a91bb8bf1cec7ce5a2c6492b
likithreddy29 <57021218+likithreddy29@users.noreply.github.com>
2026-07-30 20:43:13 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -15,3 +15,36 @@ variable "environment" {
   default     = "testing"
   description = "Deployment environment tag"
 }
+
+
+## Adding ECS fargate and networking resources
+
+variable "aws_region" {
+  description = "AWS region for provisioning resources"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "repository_name" {
+  description = "Name of the ECR repository"
+  type        = string
+  default     = "zomato"
+}
+
+variable "app_port" {
+  description = "Port exposed by the Docker container"
+  type        = number
+  default     = 3000
+}
+
+variable "fargate_cpu" {
+  description = "Fargate instance CPU units (256 = 0.25 vCPU)"
+  type        = string
+  default     = "256"
+}
+
+variable "fargate_memory" {
+  description = "Fargate instance memory (in MiB)"
+  type        = string
+  default     = "512"
+}
